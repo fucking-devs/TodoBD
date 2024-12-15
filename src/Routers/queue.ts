d2b914a583e026886cf57f54166fbc4c15f8adf2
magomed-r <magomed-radzhabov@internet.ru>
2024-12-15 20:44:52 +0300

    fix: upgrade code

---
+++ src/Routers/queue.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+import Queue from '../Models/Queue'
+
+export const queueRouter = Router()
+
+/*
+TODO: Добавить методы для создания, изменения и удаления очередей
+~ Добавление задач в очередь можно реализовать как через отдельный запрос, так и через запрос изменения очереди
+ */ 
